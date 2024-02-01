27c86972306066df9115009b5e9bc3a0129d58a1
Tj (bougyman) Vanderpoel <tj@rubyists.com>
2024-02-01 00:54:02 -0600

    Adds the "view" ability by passing one or more issue ids

--- lib/linear/commands/issue/list.rb
+++ lib/linear/commands/issue/list.rb
@@ -16,18 +16,21 @@ module Rubyists
           include SemanticLogger::Loggable
           include Rubyists::Linear::CLI::CommonOptions
 
+          argument :ids, type: :array, default: [], desc: 'Issue IDs to list'
           option :mine, type: :boolean, default: true, desc: 'Only show my issues'
           option :unassigned, aliases: ['-u'], type: :boolean, default: false, desc: 'Show unassigned issues only'
           option :full, type: :boolean, aliases: ['-f'], default: false, desc: 'Show full issue details'
 
-          def call(**options)
+          def call(ids:, **options)
             logger.debug 'Listing issues'
+            return display(issues_for(options), options) if ids.empty?
 
-            display issues_for(options), options
+            display issues_for(options.merge(ids:)), options
           end
 
           def issues_for(options)
             logger.debug('Fetching issues', options:)
+            return options[:ids].map { |id| Rubyists::Linear::Issue.find(id) } if options[:ids].any?
             return Rubyists::Linear::Issue.all(filter: { assignee: { null: true } }) if options[:unassigned]
             return Rubyists::Linear::User.me.issues if options[:mine]
 
