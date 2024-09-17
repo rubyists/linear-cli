f95aa4bfdcbe01d359917a14806c9ecebbc7ee5a
Matthew Nielsen <xunker@pyxidis.org>
2024-09-17 14:57:26 -0600

    add --project filter

--- lib/linear/commands/issue/list.rb
+++ lib/linear/commands/issue/list.rb
@@ -15,20 +15,24 @@ module Rubyists
         class List
           include SemanticLogger::Loggable
           include Rubyists::Linear::CLI::CommonOptions
+          include Rubyists::Linear::CLI::Issue # for #project_for
 
           desc 'List issues'
           example [
-            '            # List your issues',
-            '--no-mine   # List The most recent 100 issues',
-            '-u          # List unassigned issues',
-            '-fu         # List unassigned issues with full details',
-            'CRY-123     # Show issue CRY-123'
+            '                      # List your issues',
+            '--no-mine             # List The most recent 100 issues',
+            '-u                    # List unassigned issues',
+            '-fu                   # List unassigned issues with full details',
+            '--project "Manhattan" # List only issues in project "Manhattan"',
+            'CRY-123               # Show issue CRY-123'
           ]
           argument :ids, type: :array, default: [], desc: 'Issue IDs to list'
           option :mine, type: :boolean, default: true, desc: 'Only show my issues'
           option :unassigned, aliases: ['-u'], type: :boolean, default: false, desc: 'Show unassigned issues only'
           option :team, aliases: ['-t'], type: :string, desc: 'Show issues for only this team'
           option :full, type: :boolean, aliases: ['-f'], default: false, desc: 'Show full issue details'
+          option :project, type: :string, aliases: ['-p'],
+                           desc: 'Show issues for only this project. Can be name, URL, ID, or - to select from a list'
 
           def call(ids:, **options)
             logger.debug 'Listing issues'
@@ -42,6 +46,13 @@ module Rubyists
             filter[:assignee] = { isMe: { eq: true } } if options[:mine]
             filter[:assignee] = { null: true } if options[:unassigned]
             filter[:team] = { key: { eq: options[:team] } } if options[:team]
+
+            if options[:project]
+              project = project_for(options[:project])
+              logger.debug('Found project', project:)
+              filter[:project] = { id: { eq: project.id } } if project
+            end
+
             filter
           end
 
