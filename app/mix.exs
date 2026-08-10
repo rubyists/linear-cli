6c07135fd8ee2d64653916842428e19c437cdcc9
ruby-automation <171511148+ruby-automation@users.noreply.github.com>
2026-08-09 22:35:42 -0400

    chore(main): release 0.8.2

--- app/mix.exs
+++ app/mix.exs
@@ -5,7 +5,7 @@ defmodule LinearCli.MixProject do
     [
       app: :linear_cli,
       # x-release-please-start-version
-      version: "0.8.1",
+      version: "0.8.2",
       # x-release-please-end
       elixir: "~> 1.20",
       start_permanent: Mix.env() == :prod,
